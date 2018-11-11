7d29ecb9670d9ae7a3e1e010fbf0c23892b20b9b
zZz0mb <42550048+zZz0mb@users.noreply.github.com>
2018-11-10 22:50:49 -0200

    Add files via upload

---
+++ images/imagews.js
@@ -0,0 +1,5 @@
+var images = [
+	{img:"images/x_deadmeat/x_pictos/d_2100.png",width:107,height:50},
+	{img:"images/x_deadmeat/x_pictos/s_1160.png",width:86,height:84},
+	{img:"images/x_deadmeat/x_pictos/d_4708.png",width:380,height:286},
+];
